0a14be1d578378601c164688bada9e988ed0acbf
Zhang Erning <zhang@erning.com>
2010-07-28 05:44:37 +0800

    chunked base64

--- edit-patterns.py
+++ edit-patterns.py
@@ -7,30 +7,28 @@ import os
 fn_base64 = 'patterns.base64'
 fn_plain = 'patterns.txt'
 
-with open(fn_base64, 'r') as fbase64:
-    encoded = fbase64.read()
+def decode():
+    with open(fn_base64, 'r') as fbase64:
+        with open(fn_plain, 'w') as fplain:
+            base64.decode(fbase64, fplain)
+    print 'File "{0}" has been created and ready to be edit'.format(fn_plain)
 
-decoded = base64.b64decode(encoded)
+def edit():
+    editor = os.environ['EDITOR']
+    if editor == '':
+        editor = 'vi'
+    os.system('{0} {1}'.format(editor, fn_plain))
 
-with open(fn_plain, 'w') as fplain:
-    fplain.write(decoded)
+def encode():
+    with open(fn_plain, 'r') as fplain:
+        with open(fn_base64, 'w') as fbase64:
+            base64.encode(fplain, fbase64)
+    print 'File "{0}" has been created and ready to be commit'.format(fn_base64)
 
-print 'File "{0}" has been created and ready to be edit'.format(fn_plain)
+def main():
+    decode()
+    edit()
+    encode()
 
-# edit
-editor = os.environ['EDITOR']
-if editor == '':
-    editor = 'vi'
-os.system('{0} {1}'.format(editor, fn_plain))
-
-# encode
-with open(fn_plain, 'r') as fplain:
-    decoded = fplain.read()
-
-encoded = base64.b64encode(decoded)
-
-with open(fn_base64, 'w') as fbase64:
-    fbase64.write(encoded)
-
-print 'File "{0}" has been created and ready to be commit'.format(fn_base64)
-
+if __name__ == "__main__":
+    main()
